fb76a3d291d23b0c43b1a67931a17551b7259642
omeshattal <omeshattal10@gmail.com>
2022-08-31 18:27:05 +0530

    added vue from of the employees

---
+++ omesh/add-form/src/components/Form.vue
@@ -0,0 +1,47 @@
+<template>
+  <form style="margin-bottom: 50px">
+    <div class="div1">
+      <label for="id">Employee Id:</label>
+      <input style="margin-right : 20px" type="text" id="id" v-model="emp.id" />
+      <label for="fname">Employee First name:</label>
+      <input style="margin-right : 20px" type="text" id="fname" v-model="emp.firstName" />
+      <label for="lname">Employee Last name:</label>
+      <input type="text" id="lname" style="margin-right : 20px" v-model="emp.lastName" />
+      <label for="age">Employee Age:</label>
+      <input type="text" id="age" style="margin-right : 20px" v-model="emp.age" />
+      <label for="gender">Employee Gender:</label>
+      <input type="text" id="gender" style="margin-bottom : 20px" v-model="emp.gender" />
+    </div>
+    <Button BGColour="#4caf50" @submit = addEmp() name="Add Employee!"/>
+  </form>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+  name: "FormData",
+  components: {
+    Button
+  },
+  data() {
+    return {
+      emp: {}
+    }
+  },
+  methods: {
+    addEmp() {
+      this.$emit('employee', this.emp)
+      this.emp = {}
+    }
+  }
+};
+</script>
+
+<style>
+.div1 {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid black;
+  font-size: large;
+}
+</style>
---
+++ omesh/add-form/src/components/Table.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <Button BGColour="grey" @submit="submit()" name="Get Employee" style="margin-bottom: 50px" />
+
+    <table id="table1">
+      <thead>
+        <td style="border: 1px solid black;">Employee Id</td>
+        <td style="border: 1px solid black;">Employee Firstname</td>
+        <td style="border: 1px solid black;">Employee Laststname</td>
+        <td style="border: 1px solid black;">Employee Age</td>
+        <td style="border: 1px solid black;">Employee Gender</td>
+      </thead>
+      <tbody>
+        <tr :class="[index % 2 == 1 ? 'red' : 'blue']" v-for="(f, index) in data" :key="index">
+          <td :class="{border : data.length > 0}">{{ f.id }}</td>
+          <td :class="{border : data.length > 0}">{{ f.firstName }}</td>
+          <td :class="{border : data.length > 0}">{{ f.lastName }}</td>
+          <td :class="{border : data.length > 0}">{{ f.age }}</td>
+          <td :class="{border : data.length > 0}">{{ f.gender }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "FormData",
+  components: {
+    Button
+  },
+  props: {
+    data : Array
+  },
+};
+</script>
+
+<style>
+.red {
+  background-color: rgb(218, 142, 149);
+}
+.blue {
+  background-color: rgb(216, 238, 235);
+}
+.border {
+  border: 1px solid black;
+}
+table,
+th,
+td {
+  align-items: center;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+</style>
